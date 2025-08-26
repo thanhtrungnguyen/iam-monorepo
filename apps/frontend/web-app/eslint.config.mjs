1c68866e791f20781b9fcdc5dca8033031010094
Trung Nguyen Thanh <thanhtrung692k@gmail.com>
2025-08-26 11:40:26 +0700

    chore: update project configuration and dependencies
    - Update .gitignore to include 'test-output'
    - Modify ESLint configuration to use @eslint/compat for compatibility with Next.js
    - Adjust Next.js configuration to disable TypeScript checks for require statements
    - Refactor global CSS to improve formatting and remove unnecessary whitespace
    - Update tsconfig.json to streamline library references and include frontend paths
    - Add new ESLint plugins and dependencies in package.json
    - Update pnpm-lock.yaml to reflect new dependencies and versions

--- apps/frontend/web-app/eslint.config.mjs
+++ apps/frontend/web-app/eslint.config.mjs
@@ -2,6 +2,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import js from '@eslint/js';
+import { fixupConfigRules } from '@eslint/compat';
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../../eslint.config.mjs';
 const compat = new FlatCompat({
@@ -10,7 +11,8 @@ const compat = new FlatCompat({
 });
 
 export default [
-  ...compat.extends('next', 'next/core-web-vitals'),
+  ...fixupConfigRules(compat.extends('next')),
+  ...fixupConfigRules(compat.extends('next/core-web-vitals')),
   ...baseConfig,
   ...nx.configs['flat/react-typescript'],
   {
